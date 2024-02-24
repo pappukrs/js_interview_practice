1eaee62fc982d8305a89b4b156d72d44657bfce7
pappukrs <digitalpappu101@gmail.com>
2024-02-24 09:14:40 +0530

    hoisting js question done

--- day3/index.js
+++ day3/index.js
@@ -1,15 +1,49 @@
 
 
-const five=6;
-console.log(five);
+// if(6==6){
+//     console.log("if",five);
+//     let five=6;
+// }
+// else{
+//  console.log("else",five)
+// }
 
-const  Sub = (A,B) => {
 
-    return A-B ;
-}
+// const  Sub = (A,B) => {
+
+//     return A-B ;
+// }
 
 
-console.log(Sub(7,4));
+// console.log(Sub(7,4));
+
+
+
+
+
+
+//console.log(x); // undefined
+// Function Declaration
+//hoistedFunction(); // This will work
+
+// Variable Declaration
+
+// var x = 10;
+// console.log(x); // 10
+
+// function hoistedFunction() {
+//   console.log("Function Declaration is hoisted!");
+// }
+
+
+console.log(myArrowFunction); // undefined
+var myArrowFunction = () => {
+  console.log("Arrow function is not hoisted!");
+};
+
+
+
+
 
 
 
